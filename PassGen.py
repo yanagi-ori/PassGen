54dffd148e538d2c3c9cc68f51d000fcba4ec3f8
yanagi-ori <yanagiori@gmail.com>
2018-11-23 23:54:47 +0300

    Initial commit

---
+++ PassGen.py
@@ -0,0 +1,47 @@
+from tkinter import *
+
+
+class base:
+    def __init__(self):
+        self.server = root
+        self.server.destroy()
+        main_menu()
+
+
+
+def startup():
+    global pw
+    start_screen = Tk()
+    start_screen.title('PassGen - Вход')
+    start_screen.update_idletasks()
+    s = start_screen.geometry()
+    s = s.split('+')
+    s = s[0].split('x')
+    width_start_screen = int(s[0])
+    height_start_screen = int(s[1])
+    width = start_screen.winfo_screenwidth()
+    height = start_screen.winfo_screenheight()
+    width = width // 2
+    height = height // 2
+    width = width - width_start_screen // 2
+    height = height - height_start_screen // 2
+    start_screen.geometry('+{}+{}'.format(width, height))
+
+    stsc_main_text = Label(text='Для входа в программу введите ключ шифрования')
+    stsc_key = Entry(text='Рекомендуется не оставлять пустым', show='*')
+    pw = stsc_key.get()
+    stsc_button = Button(text='Вход', command=quit())
+
+    stsc_key.pack()
+    stsc_button.pack()
+    start_screen.mainloop()
+
+
+def main_menu():
+    menu = Tk()
+    menu.title('PassGen')
+    menu.mainloop()
+
+
+root = Tk()
+base()
---
+++ password.py
@@ -0,0 +1,126 @@
+# PassGen v.4.0.3
+
+from base64 import b64encode, b64decode
+from os import system
+from random import choice as random
+from sys import exit
+from tkinter import *
+
+from easygui import choicebox, passwordbox, msgbox, multpasswordbox, multenterbox
+
+
+def menu():
+    global choice
+    system('cls')
+    menu_title = 'Меню'
+    menu_msg = 'Выберите'
+    choices = ['1 - Сгенерировать случайный пароль',
+               '2 - Записать существующий',
+               '3 - Читать пароли',
+               '4 - Очистить список паролей',
+               '5 - Список изменений',
+               '0 - Выход']
+    choice = choicebox(menu_msg, menu_title, choices)
+
+
+def encoder(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return b64encode("".join(enc).encode()).decode()
+
+
+def decoder(key, enc):
+    dec = []
+    enc = b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
+root = Tk()
+e = Entry(root, width=20)
+b = Button(root, text="Преобразовать")
+l = Label(root, bg='black', fg='white', width=20)
+
+
+file_path = 'storage'
+try:
+    fp = open(file_path)
+except Exception:
+    fp = open(file_path, 'w')
+
+msg = "Введите ключ шифрования"
+title = "Пароль"
+pw = passwordbox(msg, title)
+if pw == '':
+    pw = 'empty'
+work = True
+box_button = 'Вернуться'
+name_box = 'Введите название для пароля: '
+while work:
+    menu()
+
+    if choice == '0 - Выход':
+        exit('Спасибо за использование программы')
+
+    elif choice == '1 - Сгенерировать случайный пароль':
+        storage = open('storage', 'r')
+        read = storage.read()
+        read = decoder(pw, read)
+        msg = 'Введите данные для генерации случайного пароля'
+        title = 'Генерация случайного пароля'
+        fieldNames = ['Название пароля', 'Длина пароля']
+        fieldValues = multenterbox(msg, title, fieldNames)
+        password_name = fieldValues[0]
+        chars = int(fieldValues[1])
+        password = ''.join([random(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+                            for x in range(chars)])
+        msgbox('Можете скопировать и использовать: ' + password, 'Пароль готов!', 'Далее')
+        read = read + password_name + ': ' + password + '\n'
+        storage.close()
+        storage = open('storage', 'w')
+        storage.write(encoder(pw, read))
+
+    elif choice == '2 - Записать существующий':
+        storage = open('storage', 'r')
+        read = storage.read()
+        read = decoder(pw, read)
+        storage.close()
+        storage = open('storage', 'w')
+        msg = 'Введите логин и пароль'
+        title = 'Запись существующего пароля'
+        fieldNames = ['Название пароля', 'Пароль']
+        fieldValues = multpasswordbox(msg, title, fieldNames)
+        password_name = fieldValues[0]
+        password = fieldValues[1]
+        to_encode = read + password_name + ': ' + password + '\n'
+        storage.write(encoder(pw, to_encode))
+
+    elif choice == '3 - Читать пароли':
+        storage = open('storage', 'r')
+        read = storage.read()
+        decoded = decoder(pw, read)
+        msgbox(decoded, 'Ваши пароли', box_button)
+
+    elif choice == '4 - Очистить список паролей':
+        storage = open('storage', 'w')
+        storage.write('')
+        msgbox('Ваши записи стерты', 'Очистка паролей', box_button)
+
+    elif choice == '5 - Список изменений':
+        title = 'Список изменений PassGen'
+        changelog = '4.0 - Графический интерфейс! Внедрен на 30-40% ' \
+                    '3.2 - Многочисленные фиксы - Новые пункты в меню программы ' \
+                    '3.1 - Добавлена функция очистки данных (-1 в меню) ' \
+                    '3.0 - Введено шифрование - Временный отказ от чтения ' \
+                    '2.1 - Пароль для входа в программу - Меню в отдельном файле ' \
+                    '- Незначительно улучшен код программы' \
+                    '2.0 - Меню программы - Сохранение паролей - Запись уже существующего пароля ' \
+                    '- Чтение паролей из программы' \
+                    '1.0 - Простой генератор случайного пароля'
+        msgbox(changelog, title, box_button)
