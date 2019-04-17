71affd036d196bc950b9c52b9f011d97bafb7fa7
yanagi-ori <yanagiori@gmail.com>
2019-04-17 12:00:18 +0400

    code style

--- password.py
+++ password.py
@@ -39,9 +39,9 @@ def menu():
 
 def encoder(key, clear):
     enc = []
-    for i in range(len(clear)):
-        key_c = key[i % len(key)]
-        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+    for index, item in enumerate(clear):
+        key_c = key[index % len(key)]
+        enc_c = chr(ord(item) + ord(key_c) % 256)
         enc.append(enc_c)
     return b64encode("".join(enc).encode()).decode()
 
@@ -49,9 +49,9 @@ def encoder(key, clear):
 def decoder(key, enc):
     dec = []
     enc = b64decode(enc).decode()
-    for i in range(len(enc)):
-        key_c = key[i % len(key)]
-        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+    for index, item in enumerate(enc):
+        key_c = key[index % len(key)]
+        dec_c = chr((256 + ord(item) - ord(key_c)) % 256)
         dec.append(dec_c)
     return "".join(dec)
 
